b50a12baba61dfaf8fabef594948375482bc456a
Svasti <22300286@students.ltu.edu.au>
2024-08-11 14:36:19 +1000

    server.js fortune instead of hello world

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 // Require the Express module
 const express = require('express');
+const child_process = require('child_process');
+
 // Create an Express web application instance
 const app = express();
 
 // Specify how to respond to GET /
 app.get('/', (req, res) => {
-    res.send('Hello world');
+    // Run the system `fortune` command and respond with the message
+    child_process.exec('fortune', (error, message) => {
+        if(error === null) {
+            res.send(message);
+        } else {
+            res.send('Error: ' + error);
+        }
+    })
 });
 
 // Start listening for HTTP requests on port 3000
